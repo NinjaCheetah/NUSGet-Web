550d1867279a817d5cc1cdd1359de3dabda8fa4d
NinjaCheetah <58050615+NinjaCheetah@users.noreply.github.com>
2025-02-02 16:22:11 -0500

    Begin handling basic errors, status text is now updated when things go wrong

--- scripts/app.js
+++ scripts/app.js
@@ -20,27 +20,43 @@ async function downloadTest() {
     let tgtConsole = document.getElementById("consoles").value;
     console.log(`Target Console: ${tgtConsole}`);
 
-    //let api_url = "http://localhost:5000"
-    let api_url = "https://api.nusget.ninjacheetah.dev"
+    //let api_url = "http://localhost:5000/download"
+    let api_url = "https://api.nusget.ninjacheetah.dev/download"
 
     status_text.innerHTML = "Downloading title... please wait.";
+    if (tgtConsole === "dsi") {
+        api_url += "/tad"
+    }
+    else {
+        api_url += "/wad"
+    }
 
     try {
-        const response = await fetch(api_url + `/download/wad/${tid}/${ver}`);
+        const response = await fetch(api_url + `/${tid}/${ver}`);
         if (!response.ok) {
             console.error(`Response status: ${response.status}`);
+            try {
+                let response_details = await response.json();
+                console.error(response_details);
+                status_text.innerHTML = `An error occurred. API returned: "${response_details["Error"]}"`;
+            } catch (e) {
+                console.error("No further details could be provided.")
+                status_text.innerHTML = "An unknown error occurred."
+            }
+            return;
         }
         const metadata = JSON.parse(response.headers.get("X-Metadata"));
         let api_response = await response.blob();
         const url = window.URL.createObjectURL(api_response);
         const a = document.createElement("a");
         a.href = url;
-        a.download = `${metadata["tid"]}-v${metadata["version"]}.wad`;
+        a.download = `${metadata["tid"]}-v${metadata["version"]}.${(tgtConsole === "dsi" ? "tad" : "wad")}`;
         document.body.appendChild(a);
         a.click();
         document.body.removeChild(a);
         window.URL.revokeObjectURL(url);
     } catch (e) {
         console.error(e);
+        status_text.innerHTML = "An unknown error occurred. The API may be unavailable."
     }
 }
